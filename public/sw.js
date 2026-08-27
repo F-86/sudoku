cb70e1cc64561ed8c0e84aa49eb1fcbcd46be16a
F-86 <19909233758@163.com>
2026-08-27 20:22:41 +0800

    fix: unify mobile canvas and card as pure white for seamless status bar

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v12'
+const CACHE_VERSION = 'v13'
 const CACHE_PREFIX = 'sudoku-'
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`
 const BASE_PATH = new URL(self.registration.scope).pathname
